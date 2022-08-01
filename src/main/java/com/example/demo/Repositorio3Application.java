498767d4eb51e949f4c914d95e75370ccbaa129c
wendyjazmin <wendyjazmin1994@hotmail.com>
2022-08-01 15:48:08 -0500

    repositorio3

---
+++ src/main/java/com/example/demo/Repositorio3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Repositorio3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Repositorio3Application.class, args);
+	}
+
+}
